5c449b4de94cbb7bac413f402b261455a6cb04f0
mhartington <mikehartington@gmail.com>
2016-11-19 20:54:43 -0800

    small changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDCsV2BaWPNYe7gKHRrsTTP4V3zKmZ6MBE';
 
 // Create a new component. This component should produce some HTML
-const App = function() {
-  return <div>Hi!</div>;
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 // JSX gets transpiled into vanilla JS.
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
